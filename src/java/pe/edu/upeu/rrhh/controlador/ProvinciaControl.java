a2972ab5f9e7715be0db399e6af51a5c9e55e513
Dka <Dka@Dka-PC>
2014-11-09 18:32:59 -0500

    sadasd

--- src/java/pe/edu/upeu/rrhh/controlador/ProvinciaControl.java
+++ src/java/pe/edu/upeu/rrhh/controlador/ProvinciaControl.java
@@ -44,6 +44,7 @@ public class ProvinciaControl extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
